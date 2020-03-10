55e7a77a8b5c1eeeb01cbaad33055e78e1946cef
Arshid <arshidbaba93@gmail.com>
2020-03-10 16:36:48 +0530

    script lists assets

---
+++ muxListAssets.py
@@ -0,0 +1,23 @@
+import os 
+import mux_python
+from mux_python.rest import ApiException
+
+#Authentication Setup
+configuration = mux_python.Configuration()
+configuration.username = os.environ.get('MUX_TOKEN_ID')
+configuration.password = os.environ.get('MUX_TOKEN_SECRET')
+
+#API Client Initialization
+assets_api = mux_python.AssetsApi(mux_python.ApiClient(configuration))
+
+#List Assets
+print("Listing Assets: \n")
+try:
+    list_assets_response = assets_api.list_assets()
+    for asset in list_assets_response.data:
+        print('Asset ID: ' + asset.id)
+        print('Status: ' + asset.status)
+        print('Duration: ' + str(asset.duration) + "\n")
+        # print(asset, "\n")
+except ApiException as e:
+    print("Exception while calling AssetsApi->list_assets: %s\n" % e)
